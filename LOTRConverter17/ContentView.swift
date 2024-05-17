44d93fce521e618dfa4ec92074514c3a6efde762
Nitin Jiwnani <86294062+Nitinjiwnani@users.noreply.github.com>
2024-05-17 15:58:39 +0530

    App icon added in assets with text color change

---
+++ LOTRConverter17/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  LOTRConverter17
+//
+//  Created by Nitin Jiwnani on 17/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+                .foregroundColor(Color.purple)
+            Button("Button"){
+                
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LOTRConverter17/LOTRConverter17App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverter17App.swift
+//  LOTRConverter17
+//
+//  Created by Nitin Jiwnani on 17/05/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverter17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
